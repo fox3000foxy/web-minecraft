46e5f355ee8fcb1a348bf4263c6cc020a82d7d17
fox3000foxy <lucas.jl.sannier@gmail.com>
2021-02-27 10:05:15 +0100

    pull from michaljaz

--- src/client/assets/scripts/invAPI/skins.js
+++
@@ -1,6 +0,0 @@
-function mcSkin() {
-	return `https://minotar.net/skin/${namePlayer.response}.png`
-}
-function tlSkin() {
-	return `https://tlauncher.org/upload/all/nickname/tlauncher_${namePlayer.response.toLowerCase()}.png`
-}
